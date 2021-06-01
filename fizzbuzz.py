6ae4e8de093d0a5f0b48f90d725647911b011afa
michaelgadda <gaddami@oregonstate.edu>
2021-05-31 21:51:38 -0700

    added enough code that would print fizz for numbers that were multiples of 3

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,18 @@
-def fizzbuzz(): 
+def fizzbuzz(return_num): 
 	list = [0]*100
-	for i in range(100):
-		list[i] = i+1 
+	for i in range(1,101):
+		
+		if (i)%3 == 0: 
+			list[i-1] = "Fizz"
+		else:
+
+			list[i-1] = i
 
 	print(list)
-	return list
 
+	if return_num == None:
+		return list
+	else: 
+		return list[return_num-1]
+
+fizzbuzz(None)
